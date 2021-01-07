8e07b364a806c6fb287d19430bbd25aba5df3253
Claudio <claudiocli.cp@gmail.com>
2021-01-07 19:22:57 +0100

    Added firebase
    Implemented firebase login and sign up. Need interaction with db. Need,
    also to improve error message dialogs.

--- webapp/javascript/Login.js
+++ webapp/javascript/Login.js
@@ -2,6 +2,24 @@ let toggleSignupButton=document.getElementById("toggle-signup");
 let loginButton=document.getElementById("login-button");
 let signupButton=document.getElementById("register-button");
 
+loginButton.addEventListener('click', ()	=>	{
+	let email=document.getElementById("mail-login-input").value;
+	let password=document.getElementById("password-login-input").value;
+
+	if (checkLoginInputs())	{
+		firebase.auth().signInWithEmailAndPassword(email, password)
+		.then((user) => {
+			// Signed in 
+			// TODO: redirect to main page
+		})
+		.catch((error) => {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			window.alert("Error "+errorCode+"\n"+errorMessage); 
+		});
+	}
+});
+
 toggleSignupButton.addEventListener('click', ()	=>	{
 	document.getElementById("mail-login-input").innerHTML
 	document.getElementById("login-div").style.display="none";
@@ -9,6 +27,39 @@ toggleSignupButton.addEventListener('click', ()	=>	{
 });
 
 signupButton.addEventListener('click', ()	=>	{
+	let email=document.getElementById("mail-input");
+	let password=document.getElementById("password-input");
+	
+	// TODO: register new user in db;
+	
+	if (checkRegisterInputs())	{
+		firebase.auth().createUserWithEmailAndPassword(email,
+		 password).then((user) => {
+			// Signed in 
+			// TODO: redirect to main page
+		})
+		.catch((error) => {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			window.alert("Error "+errorCode+"\n"+errorMessage);    
+		});
+	}
+});
+
+function checkLoginInputs()	{
+	let passwordField=document.getElementById("password-login-input");
+	let mailField=document.getElementById("mail-login-input");
+	
+	// TODO: check with database
+	if (false)	{
+		return false;
+	}
+	return true;
+}
+
+function checkRegisterInputs()	{
+	let returnValue=true;
+	
 	let passwordField=document.getElementById("password-input");
 	let password=passwordField.value;
 
@@ -21,28 +72,36 @@ signupButton.addEventListener('click', ()	=>	{
 
 	if (nameField.value.length==0)	{
 		nameField.style.borderColor="red";
+		returnValue=false;
 	}
 	if (surnameField.value.length==0)	{
 		surnameField.style.borderColor="red";
+		returnValue=false;
 	}
 	if (mailField.value.length==0) {
 		mailField.style.borderColor="red";
+		returnValue=false;
 	}
 	if(!passwordFormatCheck(password))	{
 		document.getElementById("password-hint").style.display="block";
 		passwordField.style.borderColor="red";
+		returnValue=false;
 	}
 	// TODO: cf validation
 	if (cfField.value.length==0) {
 		cfField.style.borderColor="red";
+		returnValue=false;
 	}
 	if (addressField.value.length==0) {
 		addressField.style.borderColor="red";
+		returnValue=false;
 	}
 	if (!validDateOfBirth(dobField.value)) {
 		dobField.style.borderColor="red";
+		returnValue=false;
 	}
-});
+	return returnValue;
+}
 
 function passwordFormatCheck(password)	{
 	let patternMaius="[A-Z]+";
