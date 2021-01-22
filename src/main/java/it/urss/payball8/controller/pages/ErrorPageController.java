964744a6247c3a0d624f08dd0c88b2fb084c3600
Claudio <claudiocli.cp@gmail.com>
2021-01-22 13:24:43 +0100

    Added error pages
    Added error pages, need to review ErrorPagesController. Plus, minor fix: specified it language

---
+++ src/main/java/it/urss/payball8/controller/pages/ErrorPageController.java
@@ -0,0 +1,41 @@
+package it.urss.payball8.controller.pages;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorPageController implements ErrorController {
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        // get error status
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        // TODO: log error details here
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            // display specific error page
+            if (statusCode == HttpStatus.NOT_FOUND.value())
+				return "404";
+			else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value())
+				return "500";
+			else if (statusCode == HttpStatus.FORBIDDEN.value())
+				return "403";
+        }
+
+        // display generic error
+        return "error";
+    }
+}
--- src/main/webapp/WEB-INF/jsp/Home.jsp
+++ src/main/webapp/WEB-INF/jsp/Home.jsp
@@ -1,6 +1,6 @@
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
     <!doctype html>
-    <html lang="en">
+    <html lang="it">
 
     <head>
 
