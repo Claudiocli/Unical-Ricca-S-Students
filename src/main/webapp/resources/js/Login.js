8b7c8ff85ebf5cb4d5b8a3b8e5511e429cce76ae
Claudio <claudiocli.cp@gmail.com>
2021-01-22 16:27:56 +0100

    Cookie-session controll

--- src/main/webapp/resources/js/Login.js
+++ src/main/webapp/resources/js/Login.js
@@ -347,3 +347,9 @@ function getCookie(name) {
 function eraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+
+// Session controll
+let isLogged=getCookie("uid");
+if (isLogged)	{
+	window.location.replace(localHost+"/home");
+}
