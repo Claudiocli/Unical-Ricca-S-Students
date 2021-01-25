9f5b061c226841ae3bea6eb4226d99eefe36630d
Claudio <claudiocli.cp@gmail.com>
2021-01-25 17:49:39 +0100

    Revert "Fix on friendlist + added error handling on add friend"
    This reverts commit 450ec6fe576c7de2f21a179bdcfff537a8ba9d7f.

--- src/main/webapp/resources/js/Carte.js
+++ src/main/webapp/resources/js/Carte.js
@@ -189,6 +189,7 @@ function createEventListenerBtn(risposta){
             });   
             }
         }, false);
+        
     }
 }
 
--- src/main/webapp/resources/js/Home.js
+++ src/main/webapp/resources/js/Home.js
@@ -318,8 +318,8 @@ function popolaListaAmici() {
                     let friendRow=document.getElementById("friend-row-"+i);
                     friendRow.addEventListener('click', ()  =>  {
                         modalFriendPopup.style.display = "block";
-                        document.getElementById("friend-id-info").innerHTML="ID:    "+id;
-                        document.getElementById("friend-name-info").innerHTML="Nome:    "+name;
+                        document.getElementById("friend-id-info").innerHTML="ID: "+id;
+                        document.getElementById("friend-name-info").innerHTML="Nome: "+name;
                     });
                 }
             },
@@ -521,5 +521,26 @@ window.onclick = function(event) {
   }
 } 
 document.getElementById("delete-friend-button").addEventListener('click', ()    =>  {
-    // TODO: Call to the server to delete the friendship and delete the friend from the friendlist
+    if (confirm("Sei sicuro di voler eliminare il tuo amico?")) {
+        let idFriend=document.getElementById('friend-id-info').innerHTML;
+        idFriend=idFriend.substring(4);
+        let data =   {
+            account1: getCookie('uid'),
+            account2: idFriend,
+        };
+        $.ajax({
+            url: "http://localhost:9090/friendship/delete",
+            method: 'DELETE',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            success: function(response){
+                window.alert("Amico eliminato correttamente");
+                modalFriendPopup.style.display = "none";
+                window.location.replace(localHost+"/home");
+            },
+            error: function(error){
+                window.alert("Abbiamo riscontrato un errore, riprova");
+            }
+        });
+    }
 })
